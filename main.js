f15bfc6ada483d5c78c087bc5245197b4676ceba
Clement <clementtan@Clements-MacBook-Pro.local>
2017-10-14 12:53:00 -0500

    third commit

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
+window.onload = function() {
+    var frame = document.body.getElementsByTagName('frameset')[1].getElementsByTagName('frame');
+    frame[0].addEventListener('click', changepage('self'), false);
+    frame[1].addEventListener('click', changepage('family'), false);
+    frame[2].addEventListener('click', changepage('school'), false);
+}
+
 function changepage(type) {
-   if(type=="self") {
-        document.getElementById('information').innerHTML = "This is about my self";
+    if(type=='self'){
+        document.getElementById('change').src ="file://"+window.location.pathname.substring(0,window.location.pathname.length-11)+"frame_h.html";
     }
-    else if(type=="family") {
-        document.getElementById('information').innerHTML = "This is about my family";
+    else if(type=='family') {
+        document.getElementById('change').src ="file://"+window.location.pathname.substring(0,window.location.pathname.length-11)+"frame_family.html";
     }
-    else if(type=="school") {
-        document.getElementById('information').innerHTML = "This is about my school";
+    else if(type=='school') {
+        document.getElementById('change').src ="file://"+window.location.pathname.substring(0,window.location.pathname.length-11)+"frame_school.html";
     }
-    return false;
+    alert("We are here");
 }
